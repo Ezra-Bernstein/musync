25612adbd1dbdf1496b2362318d3b05d2e7514b9
Ayush Varshney <34279863+ayush314@users.noreply.github.com>
2020-09-12 19:46:20 -0400

    ./tmp/

--- mp4merger.py
+++ mp4merger.py
@@ -23,36 +23,38 @@ def merge2(f1, f2):
     s1 = AudioSegment.from_wav(f1)
     s2 = AudioSegment.from_wav(f2)
     temp = s1.overlay(s2, position=0)
-    temp.export("mixed.wav", format="wav")
+    temp.export("./tmp/mixed.wav", format="wav")
 
 #merge one cut file into the main wav file
 def merge1(f1):
     s1 = AudioSegment.from_wav(f1)
-    st = AudioSegment.from_wav("mixed.wav")
+    st = AudioSegment.from_wav("./tmp/mixed.wav")
     temp = s1.overlay(st, position=0)
-    temp.export("mixed.wav", format="wav")
+    temp.export("./tmp/mixed.wav", format="wav")
 
 #everything
 def mp4merger(fnames):    
     #preprocessing (mp4 to wav)
     #fnames = ["v1.mp4","v2.mp4","v3.mp4"]
     for i in range(len(fnames)):
-        subprocess.call(['-i', os.getcwd()+"\\tmp\\"+fnames[i], os.getcwd()+"\\tmp\\"+str(i)+".wav"])
+        subprocess.call(['C:\\Users\\ayush\\Documents\\Extra\\Random Projects\\pennapps2020\\ffmpeg-20200831-4a11a6f-win64-static\\ffmpeg-20200831-4a11a6f-win64-static\\bin\\ffmpeg','-i', os.getcwd()+"\\tmp\\"+fnames[i], os.getcwd()+"\\tmp\\"+str(i)+".wav"])
   
     #cut both to start at clap
     times = []
     secs = []
     for i in range(len(fnames)):
-        fs, x = read(str(i)+".wav")
-        times.append(cutter(str(i)+".wav"))
+        fs, x = read("./tmp/"+str(i)+".wav")
+        times.append(cutter("./tmp/"+str(i)+".wav"))
         secs.append(times[i]/fs)
-        write(".\\tmp\\str(i)+"_clipped.wav", fs, x[times[i]:])
+        write("./tmp/"+str(i)+"_clipped.wav", fs, x[times[i]:])
 
     #create a combined mixed.wav file
-    merge2("\\tmp\\0_clipped.wav","\\tmp\\1_clipped.wav")
+    merge2("./tmp/0_clipped.wav", "./tmp/1_clipped.wav")
     for i in range(2, len(fnames)):
-        merge1("\\tmp\\"+str(i)+"_clipped.wav")
+        merge1("./tmp/"+str(i)+"_clipped.wav")
 
     #cut the video portions of original files and save with "new_" before the original name
     for i in range(len(fnames)):
-        subprocess.call(['-i', os.getcwd()+"\\tmp\\"+fnames[i], '-ss', str(secs[i]), os.getcwd()+"\\tmp\\new_"+fnames[i]])
+        subprocess.call(['C:\\Users\\ayush\\Documents\\Extra\\Random Projects\\pennapps2020\\ffmpeg-20200831-4a11a6f-win64-static\\ffmpeg-20200831-4a11a6f-win64-static\\bin\\ffmpeg','-i', os.getcwd()+"\\tmp\\"+fnames[i], '-ss', str(secs[i]), os.getcwd()+"\\tmp\\new_"+fnames[i]])
+
+mp4merger(["v1.mp4","v2.mp4","v3.mp4"])         
