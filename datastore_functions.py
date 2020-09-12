a3f726fccb7f881434a69f872500803d2cfbac6f
GabrielDWu <gabriel.d.wu314@gmail.com>
2020-09-11 23:32:43 -0400

    Update datastore_functions.py

--- datastore_functions.py
+++ datastore_functions.py
@@ -11,9 +11,22 @@ def addClass(classcode):
 
 def addUserToClass(classcode, username, instrument):
     datastore_client = datastore.Client()
-    key = datastore_client.key('Class', instrument)
+    key = datastore_client.key('Class', classcode)
     task = datastore_client.get(key)
     task[username] = instrument
 
     datastore_client.put(task)
 
+def addUser(username, password):
+    if "." in username:
+        return False
+    
+    datastore_client = datastore.Client()
+    task = datastore.Entity(datastore_client.key('Username', username))
+    task.update({
+        'Username': username,
+        'Password': password,
+        'Classes': []
+        })
+
+    datastore_client.put(task)
