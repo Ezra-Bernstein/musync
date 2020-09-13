1b8f10360f411f9adbfb7ed9e4a9f40db5bc980a
GabrielDWu <gabriel.d.wu314@gmail.com>
2020-09-12 20:43:19 -0400

    Update storage_functions.py

--- storage_functions.py
+++ storage_functions.py
@@ -1,6 +1,5 @@
 from google.cloud import storage
 
-
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # bucket_name = ".appspot.com"
@@ -18,3 +17,18 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
             source_file_name, destination_blob_name
         )
     )
+
+def transfer_files(bucket_name, classcode):
+    """Transfers all files in class to /tmp"""
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name)
+    count = 0
+    for blob in blobs:
+        if blob.name[:len(classcode)] == classcode:
+            blob.download_to_filename("tmp/"+classcode+"/"+blob.name[len(classcode):])
+            
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print("Downloaded",count,"files from class",classname)
